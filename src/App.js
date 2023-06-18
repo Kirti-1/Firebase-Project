2bfbe496bc6f4fe00dc7954e67f065b399adc8fe
arorakirti2212 <arorakirti2212@gmail.com>
2023-06-19 02:13:09 +0530

    Firebase Practice first commit

---
+++ src/App.js
@@ -0,0 +1,129 @@
+import "./styles.css";
+import React from "react";
+
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+      list: []
+    };
+  }
+
+  saveToDb = () => {
+    const { name, email, password } = this.state;
+    console.log(name, email, password);
+
+    firebase.firestore().collection("userDirectory").add({
+      name,
+      email,
+      password
+    });
+
+    this.setState({
+      name: "",
+      email: "",
+      password: ""
+    });
+  };
+
+  updatePassword = (event) => {
+    this.setState({
+      password: event.target.value
+    });
+  };
+
+  updateEmail = (event) => {
+    this.setState({
+      email: event.target.value
+    });
+  };
+
+  updateName = (event) => {
+    this.setState({
+      name: event.target.value
+    });
+  };
+  componentDidMount() {
+    firebase
+      .firestore()
+      .collection("userDirectory")
+      .onSnapshot((snapshot) => {
+        const list = snapshot.docs.map((doc) => {
+          let data = doc.data().name;
+          return data;
+        });
+        this.setState({
+          list
+        });
+      });
+  }
+
+  render() {
+    const { name, email, password, list } = this.state;
+    // console.log(name, email, password);
+
+    return (
+      <div className="App">
+        <form>
+          <label htmlFor="name">Name:</label>
+          <br />
+          <input
+            type="text"
+            id="name"
+            name="name"
+            onChange={this.updateName}
+            value={name}
+          />
+          <br />
+          <label htmlFor="email">Email:</label>
+          <br />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            onChange={this.updateEmail}
+            value={email}
+          />
+          <br />
+          <label htmlFor="password">Password:</label>
+          <br />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            onChange={this.updatePassword}
+            value={password}
+          />
+          <br />
+          <br />
+          <p
+            onClick={this.saveToDb}
+            style={{
+              border: "1px solid black",
+              width: "100px",
+              margin: "auto",
+              cursor: "pointer"
+            }}
+          >
+            Submit
+          </p>
+        </form>
+        <div>
+          <p>List Of User</p>
+          {list.map((l) => {
+            return <p>{l}</p>;
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBrpNkOJzIO0ES9Ffh7mxPxheurmK56g9g",
+  authDomain: "cart-5a688.firebaseapp.com",
+  projectId: "cart-5a688",
+  storageBucket: "cart-5a688.appspot.com",
+  messagingSenderId: "397797702878",
+  appId: "1:397797702878:web:f819a91581c1b38e93964f"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
