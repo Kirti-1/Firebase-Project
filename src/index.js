50f0be88ad8d0e0acf07959554f35a0db0778f9f
arorakirti2212 <arorakirti2212@gmail.com>
2023-06-19 02:18:51 +0530

    Hide the firebase details

--- src/index.js
+++ src/index.js
@@ -9,13 +9,18 @@ import "firebase/compat/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+/**
+ * Create a firebase account and get all the below details from Project Settings in Project overview
+ * 
+ */
 const firebaseConfig = {
-  apiKey: "AIzaSyBrpNkOJzIO0ES9Ffh7mxPxheurmK56g9g",
-  authDomain: "cart-5a688.firebaseapp.com",
-  projectId: "cart-5a688",
-  storageBucket: "cart-5a688.appspot.com",
-  messagingSenderId: "397797702878",
-  appId: "1:397797702878:web:f819a91581c1b38e93964f"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId:"",
+  appId: ""
 };
 
 // Initialize Firebase
